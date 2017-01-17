726200a7b475617fcdee68aa9bd46d69b9136218
sztinq <tinqflower@sina.com>
2017-01-17 21:37:27 +0800

    Update settings.py
    change the mysql's passwd

--- My_Projects/blog_project/blog_project/settings.py
+++ My_Projects/blog_project/blog_project/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog',
         'USER': 'root',
-        'PASSWORD':'1234qwer',
+        'PASSWORD':'××××××',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
